3ffd3a408207c57569d03a251575e8ec7d048cc8
Felix <felixletsplay@outlook.de>
2020-11-17 22:00:50 +0100

    :sparkles: Add login mechanism

--- webserver.py
+++ webserver.py
@@ -1,17 +1,50 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, redirect, request, session
 import json
+
 import dbs
+import hashlib
 from datetime import datetime
 app = Flask(__name__)
+app.secret_key = os.getenv('UPTIME_MONITOR_SECRET')
 
 
 @app.route('/')
 def index():
-    return redirect('/downtimes')
+    if session is not None and 'authenticated' in session and session['authenticated']:
+        return redirect('/downtimes')
+    return redirect('/login')
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html.twig')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    if 'username' not in request.form or 'password' not in request.form:
+        return redirect('/login', 400)
+    if request.form['username'] == 'admin' and \
+         hashlib.sha1(request.form['password'].encode()).hexdigest() == os.getenv('UPTIME_MONITOR_PASSWORD'):
+        session['authenticated'] = True
+        return redirect('/downtimes')
+    else:
+        session['authenticated'] = False
+        return redirect('/', 403)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['authenticated'] = False
+    return redirect('/')
 
 
 @app.route('/downtimes')
 def downtimes():
+    if not session['authenticated'] is True:
+        return redirect('/login', 403)
     db = dbs.DB()
     data = db.get_unsuccessful_connections_today()
     template_data = {}
@@ -25,8 +58,11 @@ def downtimes():
 
 @app.route('/api/downtimes')
 def downtimes_api():
+    if not session['authenticated'] is True:
+        return redirect('/login', 403)
     db = dbs.DB()
     return json.dumps(db.get_unsuccessful_connections_today())
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
