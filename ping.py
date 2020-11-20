08fe62705d58f59b78a8a47bd83d3c3206e1c836
Felix <felixletsplay@outlook.de>
2020-11-20 17:05:43 +0100

    :bug: Catch Operational Error in ping.py
    This would occasional crash the Ping script, because
    of the usage of sqlite

--- ping.py
+++ ping.py
@@ -1,6 +1,7 @@
 import dbs
 from ping3 import ping
 import time
+import sqlite3
 
 
 db = dbs.DB()
@@ -13,24 +14,27 @@ nextExecution = {HOST_EXECUTION_ID: 0}
 print("Started. Going into connection mode")
 
 while True:
-    # Update chached Host Data
-    if nextExecution[HOST_EXECUTION_ID] < time.time():
-        hosts = db.get_hosts(only_active=True)
-        nextExecution[HOST_EXECUTION_ID] = time.time() + 10
-    # Try connections
-    for host in hosts:
-        hostID = host['id']
-        if hostID not in nextExecution:
-            nextExecution[hostID] = 0
-        if nextExecution[hostID] > time.time():
-            continue  # Wait until connection should be tried
-        # Set next execution first
-        nextExecution[hostID] = time.time() + host['interval']
-        if host['type'] == 'ping':
-            c = time.time()
-            t = ping(host['host'])
-            if t is False:
-                db.insert_unsuccessful_ping(host['id'], c)
-            else:
-                db.insert_successful_ping(host['id'], c, t)
+    try:
+        # Update chached Host Data
+        if nextExecution[HOST_EXECUTION_ID] < time.time():
+            hosts = db.get_hosts(only_active=True)
+            nextExecution[HOST_EXECUTION_ID] = time.time() + 10
+        # Try connections
+        for host in hosts:
+            hostID = host['id']
+            if hostID not in nextExecution:
+                nextExecution[hostID] = 0
+            if nextExecution[hostID] > time.time():
+                continue  # Wait until connection should be tried
+            # Set next execution first
+            nextExecution[hostID] = time.time() + host['interval']
+            if host['type'] == 'ping':
+                c = time.time()
+                t = ping(host['host'])
+                if t is False:
+                    db.insert_unsuccessful_ping(host['id'], c)
+                else:
+                    db.insert_successful_ping(host['id'], c, t)
+    except sqlite3.OperationalError:
+        print("Could not read or write because of Database locking, skip!")
     time.sleep(0.1)
